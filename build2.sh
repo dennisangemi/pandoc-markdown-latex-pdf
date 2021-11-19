becc95e99bef4c13c44e1286c8f5f20fdbdcaa21
Dennis Angemi <dennisangemi@gmail.com>
2021-11-19 20:33:45 +0100

    added build 2

---
+++ build2.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+### requisiti ###
+# pandoc 
+# xelatex 
+### requisiti ###
+
+#leggere questo https://www.cyberciti.biz/faq/bash-loop-over-file/
+
+CONTENTDIR="content"
+BUILDDIR="build"
+ASSETSDIR="assets"
+
+git pull
+echo "Updated!"
+
+# Elimina la cartella con i file di output
+rm -r "${BUILDDIR}"
+
+# Crea la cartella con i file di output
+mkdir "${BUILDDIR}" -p
+echo "Creating PDF print output"
+
+# Ciclo che converte ogni md in pdf
+for filename in content/*.md; do
+pandoc "$filename" \
+     --resource-path="${CONTENTDIR}" \
+     --citeproc \
+     --csl="${ASSETSDIR}/citation-style.csl" \
+     --from="markdown+tex_math_single_backslash+tex_math_dollars+raw_tex" \
+     --to="latex" \
+     --output="${BUILDDIR}/$filename.pdf" \
+     --pdf-engine="xelatex" \
+     --include-in-header="layouts/print.tex"
+done
+
+echo "Done!"
+
+# Lo script originario era questo
+#pdf_print() {
+#    mkdir "${BUILDDIR}" -p
+#    echo "Creating PDF print output"
+#    pandoc "${CONTENTDIR}/${FILENAME}.md" \
+#        --resource-path="${CONTENTDIR}" \
+#        --citeproc \
+#        --csl="${ASSETSDIR}/citation-style.csl" \
+#        --from="markdown+tex_math_single_backslash+tex_math_dollars+raw_tex" \
+#        --to="latex" \
+#        --output="${BUILDDIR}/output_print.pdf" \
+#        --pdf-engine="xelatex" \
+#       --include-in-header="layouts/print.tex"
+#}
---
+++ content/test.md
@@ -0,0 +1,38 @@
+---
+title: "Appunti di Fisica Generale I"
+date: "2021-11-18"
+author: "Dennis Angemi"
+bibliography: "bibliography.bib"
+link-citations: true
+urlcolor: "blue"
+---
+
+# Campo gravitazionale
+
+Definiamo il campo gravitazionale come 
+$$
+⃗\nabla g(r) = \frac{1}{r^2}
+$$
+
+Nota bene: ho accettato un suggerimento random. Lo sappiamo che quella non è la definizione di campo gravitazionale
+
+# Angolo solido
+
+$$
+f(a)=\frac{1}{2πi}∮_γ\frac{f(z)}{z-a}\,dz.
+\tag{1}
+\label{eq:1}
+$$
+
+As seen in equation $\eqref{eq:1}$, duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+As seen in equation $\eqref{eq:2}$ test
+
+# Teorema di Gauss per la gravitazione
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+$$
+f(a)=\frac{1}{2πi}∮_γ\frac{f(z)}{z-a}\,dz.
+\tag{1}
+\label{eq:1}
+$$
